e5da64329615b38c8feccf76a4106fe95bd321bd
Yang Tian <108443106+yangtian6781@users.noreply.github.com>
2024-07-21 20:09:31 +0800

    Update analyze.py

--- analyze.py
+++ analyze.py
@@ -1,15 +1,17 @@
 import matplotlib.pyplot as plt
 import re
-def draw_loss_curve(log_path):
+def draw_loss_curve(log_path, key_strs=['loss']):
     with open(log_path, 'r') as f:
         a = f.readlines()
-        a = filter(lambda x:'loss' in x or 'lr' in x, a)
-        a = list(map(lambda x:x[:-1], a))
-        pattern_loss = re.compile(r'loss:(\d+\.\d+)')
-        loss = list(map(lambda x:float(pattern_loss.findall(x)[0]), a))
-        plt.plot(loss)
-        plt.xlabel('Steps')
-        plt.ylabel('Loss')
-        plt.title('Training Loss Over Steps')
-        plt.grid(True)
-        plt.show()
+        for key_str in key_strs:
+            a = filter(lambda x:'loss' in x or 'lr' in x, a)
+            a = list(map(lambda x:x[:-1], a))
+            pattern_loss = re.compile(f'{key_str}:(\d+\.\d+)')
+            loss = list(map(lambda x:float(pattern_loss.findall(x)[0]), a))
+            key_str = key_str.title()
+            plt.plot(loss)
+            plt.xlabel('Steps')
+            plt.ylabel(f'{key_str}')
+            plt.title(f'Training {key_str} Over Steps')
+            plt.grid(True)
+            plt.show()
