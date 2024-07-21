0cea951de58e329032864a2e3e90d69a58d7dc2b
Yang Tian <108443106+yangtian6781@users.noreply.github.com>
2024-07-21 09:32:30 +0800

    add lr

--- loggger.py
+++ loggger.py
@@ -7,7 +7,7 @@ import torch
 from torch.distributed import get_rank
 
 class Loggger:
-    def __init__(self, root='./', epoch=1, iter_per_epoch=1, log_per_iter=50, multi_rank=False, logger = logging.getLogger('train'), dictory_name='hello'):
+    def __init__(self, root='./', epoch=1, iter_per_epoch=1, log_per_iter=50, multi_rank=False, optimizer=torch.optim.AdamW(), logger = logging.getLogger('train'), dictory_name='hello'):
         self.root_dirctory = root+'work_dir/'
         self.project_path = self.root_dirctory + dictory_name + '/'
         if not os.path.exists(self.project):
@@ -17,6 +17,7 @@ class Loggger:
         self.current_epoch = 1
         self.iter_per_epoch = iter_per_epoch
         self.iterations = 0
+        self.optimizer = optimizer
         self.logger = logger
         self.log_per_iter = log_per_iter
         self.current_iter = 0
@@ -46,13 +47,14 @@ class Loggger:
         self.process_input(message)
         self.iterations += 1
         self.current_iter += 1
+        self.lr = f'     lr: {self.optimizer.param_groups[0]['lr']}'
         if self.multi_rank:
             prefix = f"epoch:[{f'{self.current_epoch}/{self.epoch}':8}][{f'{self.current_iter}/{self.iter_per_epoch}':13}] rank:{self.rank}   "
         else:
             prefix = f"epoch:[{f'{self.current_epoch}/{self.epoch}':8}][{f'{self.current_iter}/{self.iter_per_epoch}':13}]   "
         if self.current_iter%self.log_per_iter == 0:
-            self.logger.info(prefix + self.process_buffer(), **kwargs)
+            self.logger.info(prefix + self.process_buffer() + self.lr, **kwargs)
         if self.current_iter == self.iter_per_epoch:
-            self.logger.info(prefix + self.process_buffer(), **kwargs)
+            self.logger.info(prefix + self.process_buffer() + self.lr, **kwargs)
             self.current_iter=0
             self.current_epoch += 1
